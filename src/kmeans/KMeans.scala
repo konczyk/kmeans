8295cc7e6f321ca86824be34b2533696ce34c298
Daniel Kończyk <daniel@konczyk.net>
2017-03-01 04:04:24 +0100

    Implement k-means++ strategy

--- src/kmeans/KMeans.scala
+++ src/kmeans/KMeans.scala
@@ -22,11 +22,42 @@ object KMeans {
     go(Set(), IndexedSeq())
   }
 
+  // k-means++ algorithm
+  def kppCentroids: CentroidStrategy = (k, pts, seed) => {
+    val rnd = rand(seed)
+    val first = rnd.nextInt(pts.length)
+    def go(sample: PointSeq): PointSeq = {
+      if (sample.size == k) sample
+      else {
+        val next = probSample(pts, sample, rnd.nextDouble)
+        go(sample :+ pts(next))
+      }
+    }
+    go(IndexedSeq(pts(first)))
+  }
+
+  // sample a point using probability mass function, where probability
+  // associated with each point is proportional to its distance from the nearest
+  // centroid and a random prob value
+  private def probSample(points: PointSeq, centroids: PointSeq, prob: Double) = {
+    val distances = points.map(
+      p => centroids.foldLeft(Double.MaxValue)(_ min p.distanceTo(_))
+    )
+    val total = distances.sum
+    def go(idx: Int, acc: Double): Int = {
+      if (prob <= acc) idx
+      else go(idx+ 1, acc + distances(idx+1)/total)
+    }
+    go(0, distances.head/total)
+  }
+
 }
 
 class KMeans(seed: Option[Long] = None) {
 
-  def init(k: Int, points: PointSeq, initStrategy: CentroidStrategy): PointSeq =
+  def init(k: Int, points: PointSeq, initStrategy: CentroidStrategy): PointSeq = {
+    require(k > 0)
     initStrategy(k, points, seed)
+  }
 
 }
--- test/KMeansTest.scala
+++ test/KMeansTest.scala
@@ -8,18 +8,26 @@ import KMeans._
 @RunWith(classOf[JUnitRunner])
 class KMeansTest extends FunSuite {
 
-  val pts1 = "0,0 4,5 2,3 9,6 8,2 -9,2 8,6 4,3 2,8"
+  val points = "0,0 4,5 2,3 9,6 8,2 -9,2 8,6 4,3 2,8"
 
   def parsePoints(pts: String): PointSeq =
-    pts1.split("\\s+").map(_.split(",").map(_.toDouble)).map(DataPoint(_))
+    pts.split("\\s+").map(_.split(",").map(_.toDouble)).map(DataPoint(_))
 
   def pt(p: Double*) = DataPoint(p.toSeq)
 
   test("random centroids") {
-    val pts = parsePoints(pts1)
+    val pts = parsePoints(points)
     val seed = Some(5L)
 
     assert(randomCentroids(3, pts, seed) === Seq(pt(2,8), pt(4,5), pt(-9,2)))
   }
+
+  test("kmeans++ centroids") {
+    val pts = parsePoints(points)
+    val seed = Some(3L)
+
+    assert(kppCentroids(3, pts, seed) === Seq(pt(-9,2), pt(8,2), pt(2,8)))
+  }
+
 
 }
